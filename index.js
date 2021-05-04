7f6187206c5c1a2b6a75fb7d57eb7edeb13d005c
ghemsley <ghemsley@protonmail.ch>
2021-05-04 15:45:49 -0500

    commit tree 410ade6cc7af7f1b9b12b3e7d949872d18833b8e

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+const dodger = document.getElementById('dodger')
+document.addEventListener('keydown', (e) => {
+  if (e.key == 'ArrowLeft') {
+    moveDodgerLeft()
+  }
+})
+
+document.addEventListener('keydown', (e) => {
+  if (e.key == 'ArrowRight') {
+    moveDodgerRight()
+  }
+})
+
+const moveDodgerLeft = () => {
+  let leftNumbers = dodger.style.left.replace('px', '')
+  let left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+const moveDodgerRight = () => {
+  let leftNumbers = dodger.style.left.replace('px', '')
+  let left = parseInt(leftNumbers, 10)
+
+  if (left < window.innerWidth + dodger.style.width) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
